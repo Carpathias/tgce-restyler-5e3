120f6a54630eb7eb052c1066e3827af12dae29d1
Carpathias <79472577+Carpathias@users.noreply.github.com>
2024-02-26 23:14:19 -0500

    update gitignore

---
+++ beta/client-side-css-update.js
@@ -0,0 +1,6 @@
+ 
+//  static _pinkStyleOnOff() {
+//      const pinkSetting = game.settings.get(MODULE, 'pinkTheme') ?? []
+//      const link = `<link rel="stylesheet" href="/modules/mymodule/styles/modules/pink-theme.css">`
+//      if (pinkSetting) document.head.insertAdjacentHTML('beforeend', link);
+//    };
